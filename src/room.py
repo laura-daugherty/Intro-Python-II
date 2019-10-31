ed62276c9370e16b45326559a9a7c0ca53f4ea40
Laura Daugherty <lauradaugherty6@gmail.com>
2019-10-30 18:09:06 -0700

    init room class

--- src/room.py
+++ src/room.py
@@ -1,2 +1,11 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+class Room:
+  def __init__(self, name, desc):
+    self.name = name
+    self.desc = desc
+    self.n_to = ""
+    self.e_to = ""
+    self.s_to = ""
+    self.w_to = ""
