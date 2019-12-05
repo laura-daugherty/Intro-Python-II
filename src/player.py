f6a2abeccabbea4d8d6d79520df974cd6eb22bf4
Laura Daugherty <lauradaugherty6@gmail.com>
2019-12-05 15:52:28 -0800

    add and remove points method, adding points to pla

--- src/player.py
+++ src/player.py
@@ -5,12 +5,14 @@ class Player:
     self.name = name
     self.current_room = current_room
     self.items = []
+    self.points = 0
 
   
   def __str__(self):
     r = f"{self.name} has these items:\n"
     for n in enumerate(self.items, start = 1):
       r += f"    {n}\n"
+    print(f'{self.name} has this many points: {self.points}')
     return r
 
   def __repr__(self): # generally for programmer consumption
@@ -21,5 +23,11 @@ class Player:
 
   def removeItem(self, item):
     self.items.remove(item)
+
+  def addPoints(self, point):
+    self.points = self.points + point
+
+  def removePoints(self, point):
+    self.points = self.points - point
 
 
