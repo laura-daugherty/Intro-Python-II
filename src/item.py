e668925f0c6fb297655c1b0a8c90a10452d47c5e
Laura Daugherty <lauradaugherty6@gmail.com>
2019-12-05 10:47:21 -0800

    adding __str__ and __repr__ to item class

--- src/item.py
+++ src/item.py
@@ -3,4 +3,9 @@ class Item:
     self.name = name
     self.desc = desc
 
+  def __str__(self):
+    return f"name: {self.name} "
+
+  def __repr__(self): # generally for programmer consumption
+    return f'name: {self.name}'
   
